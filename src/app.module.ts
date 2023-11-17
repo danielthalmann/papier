688473bde82cb0d9b43371fb820818719a78d1b2
Daniel Thalmann <daniel@thalmann.li>
2023-11-17 16:07:42 +0100

    add static page

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,21 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from './prisma.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 
 @Module({
   imports: [
     ConfigModule.forRoot({envFilePath: '.env'}),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
     AuthModule, 
     UsersModule, 
   ],
-  controllers: [AppController],
+  //controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
