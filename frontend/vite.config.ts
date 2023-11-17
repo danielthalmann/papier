f8627469894531f0f9b02bd9d090a8ef41e77a79
Daniel Thalmann <daniel@thalmann.li>
2023-11-18 00:27:05 +0100

    login form in skelt

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,5 +5,14 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+	},
+    server: {
+        port: 3001,
+        host: true,
+        strictPort: true,
+		// this part of configuration is needed to docker
+		watch : {
+			usePolling: true
+		}
+    }
 });
