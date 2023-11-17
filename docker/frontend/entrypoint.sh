203878c58efc8e8452b9720fc4030063875bcf69
Daniel Thalmann <daniel@thalmann.li>
2023-11-17 23:44:50 +0100

    fix docker front

--- docker/frontend/entrypoint.sh
+++ docker/frontend/entrypoint.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 cd /opt/app
 npm install
-if [ "$APP_ENV" == "dev" ]; then
-    echo "developpement\n"
-    npm run dev
-else
-    echo "production\n"
-    npm run start
-fi
+npm run dev
